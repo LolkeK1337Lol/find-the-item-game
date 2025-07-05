92ea6d26e517296a25d145510e0cc06adf3fdce4
Daniel <daniel00345s@gmail.com>
2025-07-05 09:38:45 +0600

    feat: добавлен глобальный прелоадер при старте приложения

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -1,11 +1,26 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import PostForm from "./pages/PostForm";
 import PostDetails from "./pages/PostDetails";
 import About from "./pages/About";
 import Contacts from "./pages/Contacts";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Имитация загрузки: например, можно проверить авторизацию, подгрузить базовые данные и т.д.
+    const init = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 800)); // задержка 0.8 сек
+      setLoading(false);
+    };
+    init();
+  }, []);
+
+  if (loading) return <Spinner />;
+
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
